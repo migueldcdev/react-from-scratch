aa4fb3d88daa1ace5536df8c9e01f4597afd5507
vboxuser <vboxuser@databox.myguest.virtualbox.org>
2024-09-24 20:24:25 +0200

    added initial source files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+import { App } from "./app";
+
+const root = ReactDom.createRoot(document.getElementById('root'));
+
+root.render(
+    <App />
+)
