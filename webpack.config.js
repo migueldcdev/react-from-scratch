d9f84456489047e3adf05f170f70d3dd1dc588f1
miguel <miguel@tonio.dev>
2024-09-25 12:11:11 +0200

    added source map

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { SourceMapDevToolPlugin } = require('webpack');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: './src/index.js',   
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
@@ -24,6 +25,11 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './public/index.html'
+        }),
+        new SourceMapDevToolPlugin({            
+            test: /\.js$/,
+            exclude: /node_modules/,
         })
-    ]
+    ],
+    
 }
