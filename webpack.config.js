f0c3fad09ad18670ec0dd325f6325d1b5d111406
miguel <miguel@tonio.dev>
2024-09-25 09:57:52 +0200

    done webpack final working config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,29 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry:'./src/index.js',
+    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
-    }
+    },
+    devServer: {
+        static: './dist',
+        open: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
 }
